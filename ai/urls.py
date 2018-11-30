9a2e5c3232bfd4d57be06d293e7b98e9a83720d4
MakwanaVihas <43501553+MakwanaVihas@users.noreply.github.com>
2018-11-30 23:34:01 +0530

    Update urls.py

--- ai/urls.py
+++ ai/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns=[
 path("",views.index,name="index"),
 path("/news_of_ai=<int:id>",views.page,name="ai"),
 path("",views.index1,name="page")
 
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
