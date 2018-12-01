bf68f49481009331faa43129905e9eb98a162e08
MakwanaVihas <43501553+MakwanaVihas@users.noreply.github.com>
2018-12-01 18:24:00 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -124,7 +124,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)
+    '/var/www/static/',
+ )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
